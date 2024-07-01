148d8d22adb96ad72c2a41b8c3909a28ea5f5820
A.V. Peredery <peredery@pm.me>
2024-07-01 20:16:14 +0500

    fix(migration): set default

--- db/migrate/20240529175242_add_admin_field_to_user.rb
+++ db/migrate/20240529175242_add_admin_field_to_user.rb
@@ -2,6 +2,6 @@
 
 class AddAdminFieldToUser < ActiveRecord::Migration[7.1]
   def change
-    add_column :users, :admin, :boolean
+    add_column :users, :admin, :boolean, default: false
   end
 end
