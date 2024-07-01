6c73c87a0b9f1ad9779ab82d21cda92b95dbe34b
A.V. Peredery <peredery@pm.me>
2024-07-01 20:33:57 +0500

    refactor(flash): move to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  FLASH_CLASSES = { 'notice' => 'alert-success', 'alert' => 'alert-warning', 'error' => 'alert-danger' }.freeze
+
   def resized_image(record, width, height)
     return unless record.image.attached?
 
     record.image.variant(resize_to_limit: [width, height])
+  end
+
+  def flash_classes(type)
+    FLASH_CLASSES[type]
   end
 end
